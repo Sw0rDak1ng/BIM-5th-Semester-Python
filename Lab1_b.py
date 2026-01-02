4878548baa31eccc0a706e96104df6aa69484526
Swornim Regmi <regmiswornim2061@gmail.com>
2026-01-02 15:05:42 +0545

    Python

--- Lab1_b.py
+++ Lab1_b.py
@@ -1,9 +1,13 @@
 import math
 
-a=int(input(("Enter the first number:")))
-b=int(input(("Enter the Second number:")))
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
 
-print("math.sqrt(a)")
-print("math.sqrt(b)")
-      
-      
+x = math.sqrt(a)
+y = math.sqrt(b)
+
+print("Square root of", a, "is", x)
+print("Square root of", b, "is", y)
+
+s = x + y
+print("Sum of the square roots is", s)
