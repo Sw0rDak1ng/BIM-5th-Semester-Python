8a60661b0a23b7cd77e5794916b5a64127ecb1ac
Swornim Regmi <regmiswornim2061@gmail.com>
2026-01-02 20:18:17 +0545

    NEW COMMIT

--- Lab1_a.py
+++ Lab1_a.py
@@ -1,7 +1,14 @@
-#wap to input 2 numbers and perform add,sub,mul,div
-x=5
-y=5
-print(x+y)
-print(x-y)
-print(x*y)
-print(x/y)
+
+#Wap to input 2 numbers and perform Add,Sub,Mul and Div
+
+a=int(input('Enter the first Number:'))
+b=int(input('Enter the first Number:'))
+
+print('The Sum is:',a+b)
+
+print('The Sub is:',a-b)
+
+print('The Mul is:',a*b)
+
+print('The Div is:',a/b)
+
