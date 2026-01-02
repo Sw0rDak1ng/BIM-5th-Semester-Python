a2d79caddb1cdcb2f969b809d39238b723c165f2
Swornim Regmi <regmiswornim2061@gmail.com>
2026-01-02 15:22:33 +0545

    New commit

--- Lab1_c.py
+++ Lab1_c.py
@@ -1,16 +1,26 @@
+#Create a menu driven program
 print('1.Area of circle')
-print('2.Vowel/Consonant')
+print('2.Vowel/Consonent')
 print('3.Odd/Even')
-choice=int(input('Enter your choice(1-3)'))
+choice=int(input('Enter your choice:'))
 if choice==1:
-    radius=float(input('Enter the radius of the circle'))
+    radius=float(input('Enter the radius of the circle:'))
     area=3.14*radius*radius
     print('Area of the circle is:',area)
 
 elif choice==2:
     char=input('Enter a character:')
     if char in 'AEIOUaeiou':
-        print(char,'Vowel')
+        print('Vowel')
+    else:
+        print('consonent')
+
+elif choice==3:
+    a=int(input('Enter a number:'))
+    if a%2==0:
+        print('Even')
+    else:
+        print('odd')
 
 
 
---
+++ Lab1_d.py
@@ -0,0 +1,5 @@
+#wap to input 3 different numbers and print the middle number
+a=int('Enter the first number')
+b=int('Enter the Second number')
+c=int('Enter the Third number')
+
