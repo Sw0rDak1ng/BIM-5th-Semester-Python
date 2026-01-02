aa67afcd03db7a5c02714d42fb16329ec16e8ae4
Swornim Regmi <regmiswornim2061@gmail.com>
2026-01-02 20:33:01 +0545

    New commit

---
+++ Lab1_a.py
@@ -0,0 +1,12 @@
+#Wap to Enter 2 number and do the add,sub,min and div
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+
+
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
